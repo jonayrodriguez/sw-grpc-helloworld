44336a740f474feac43a1a62f25346bbabdccccb
jonayrodriguez <jonayrodriguez@gmail.com>
2020-10-31 19:26:36 +0000

    Adding config plus building variables

--- cmd/helloworld/main.go
+++ cmd/helloworld/main.go
@@ -1,26 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
 
-	pb "github.com/logicnow/sw-grpc-helloworld/api/helloworld"
-	hs "github.com/logicnow/sw-grpc-helloworld/internal/helloworld/server"
+	api "github.com/logicnow/sw-grpc-helloworld/api/helloworld"
+	config "github.com/logicnow/sw-grpc-helloworld/internal/helloworld/config"
+	server "github.com/logicnow/sw-grpc-helloworld/internal/helloworld/server"
+
 	"google.golang.org/grpc"
 )
 
 const (
-	address = "localhost:50051"
+	defaultConfigPath = "../../config/default.yml"
+)
+
+var (
+	version   string = "v0.0.0"
+	buildTime string = time.Now().Format(time.RFC3339)
 )
 
 func main() {
+
+	/* Building binaries with version information and other metadata will improve your monitoring, logging, and debugging processes.
+	   For example:
+	   go build argsXYX -ldflags "-X main.Version=v1.0.0 -X main.buildTime=$(date +"%Y.%m.%d.%H%M%S")"
+	*/
+	fmt.Printf("Version: %s\n", version)
+	fmt.Printf("Build Time: %s\n", buildTime)
+
+	var configPath string
+
+	if len(os.Args) < 2 {
+		configPath = defaultConfigPath
+	} else {
+		configPath = os.Args[1]
+
+	}
+	fmt.Printf("Configuration: %s\n", configPath)
+
+	conf, err := config.LoadConfiguration(configPath)
+	if err != nil {
+		log.Fatalf("Configuration failure: %v", err)
+	}
+
+	address := fmt.Sprintf("%s%s%d", conf.Server.Host, ":", conf.Server.Port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterHelloworldServer(s, hs.NewServer())
+	api.RegisterHelloworldServer(s, server.NewServer())
+
+	fmt.Printf("Listening on %s\n", address)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
-	golang.org/x/net v0.0.0-20201029221708-28c70e62bb1d // indirect
+	github.com/kelseyhightower/envconfig v1.4.0
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
 	golang.org/x/sys v0.0.0-20201029080932-201ba4db2418 // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/tools v0.0.0-20201030160639-589136c8afd9 // indirect
-	google.golang.org/genproto v0.0.0-20201029200359-8ce4113da6f7 // indirect
+	google.golang.org/genproto v0.0.0-20201030142918-24207fddd1c3 // indirect
 	google.golang.org/grpc v1.33.1
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/validator.v2 v2.0.0-20200605151824-2b28d334fa05
+	gopkg.in/yaml.v2 v2.3.0
 )
 
 replace github.com/logicnow/sw-grpc-helloworld/api/helloworld => ./api/helloworld
